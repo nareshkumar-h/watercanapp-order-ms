d6c16499dac1f667253287ed7192e372fb0590f3
janshi <janshi@gmail.com>
2019-10-24 11:24:01 +0530

    Added project setup in microservices

---
+++ src/main/java/com/revature/watercanapporderms/WatercanappOrderMsApplication.java
@@ -0,0 +1,13 @@
+package com.revature.watercanapporderms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatercanappOrderMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatercanappOrderMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/watercanapporderms/WatercanappOrderMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.watercanapporderms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatercanappOrderMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
